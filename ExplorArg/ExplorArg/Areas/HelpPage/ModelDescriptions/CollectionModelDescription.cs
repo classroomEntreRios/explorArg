6220b327a266a07ff8d1051d48fe2a26d646898c
lbdsebastian <lbdsebastian@gmail.com>
2021-03-03 15:31:11 -0300

    Estructura WebApi Visual Studio
    Signed-off-by: lbdsebastian <lbdsebastian@gmail.com>

---
+++ ExplorArg/ExplorArg/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ExplorArg.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ExplorArg/ExplorArg/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExplorArg.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
