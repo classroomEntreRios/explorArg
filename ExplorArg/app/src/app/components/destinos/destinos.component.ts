6b53fc86da366f1e3e65f41c3c7cc7fe2562074d
Sebastian Londero <xebagis@gmail.com>
2021-03-21 22:58:17 -0300

    'Navbar_proofs'

---
+++ ExplorArg/app/src/app/components/destinos/destinos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destinos',
+  templateUrl: './destinos.component.html',
+  styleUrls: ['./destinos.component.css']
+})
+export class DestinosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
