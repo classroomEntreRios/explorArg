2f6254901aa8e9a24380655277c8eee7b6c5cbc6
Sebastian Londero <xebagis@gmail.com>
2021-04-17 20:45:33 -0300

    'Saturday_fails'

--- ExplorArg/app/src/app/components/inicio/inicio.component.ts
+++ ExplorArg/app/src/app/components/inicio/inicio.component.ts
@@ -1,5 +1,12 @@
 import { RegistroService } from './../../services/registro.service';
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { DatosService } from './../../services/datos.service';
+import { Usuario } from './../../models/usuario';
+import { IngresoService } from 'src/app/services/ingreso.service';
+
+
 
 @Component({
   selector: 'app-inicio',
@@ -8,10 +15,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private registroS: RegistroService) { }
+  constructor(
+    private registroS: RegistroService,
+    private router: Router,
+    private datos: DatosService,
+    private cookieSvc: CookieService,
+    private login : IngresoService
+    ) { }
+
+  logState : boolean = false;
+  datosUsuario: Usuario[] = []
 
   ngOnInit(): void {
     this.verUsuarios();
+    this.userLog();
   }
 
   cerrarModal(){
@@ -22,5 +39,22 @@ export class InicioComponent implements OnInit {
     this.registroS.obtenerUsuarios().subscribe(data =>{
       console.log(data)
     })
+  }
+
+  userLog() {
+    let cookieString = this.cookieSvc.get('userCookie')
+    let tokenString = this.datos.mostrarToken()
+    if(tokenString == cookieString){
+      console.log('Sesión de usuario ON')
+      this.logState = true
+    }else{
+      this.cookieSvc.delete('userCookie')
+      console.log('Sesión de usuario OFF')
+      this.logState = false
+      alert('Sesión de usuario expirada')
+      setTimeout(() => {
+        this.router.navigate(['ingreso'])
+      }, 2000)
+    }
   }
 }
