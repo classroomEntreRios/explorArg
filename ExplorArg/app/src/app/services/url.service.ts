f7e91edc71cd68b154a175d3ac1d4e3b5fb503d4
lbdsebastian <lbdsebastian@gmail.com>
2021-05-10 16:41:25 -0300

    ssl fixes

--- ExplorArg/app/src/app/services/url.service.ts
+++ ExplorArg/app/src/app/services/url.service.ts
@@ -9,33 +9,33 @@ export class UrlService {
 
 
 // login y registro
-urlRegistro = 'http://explorargdb.somee.com/api/usuario';
-urlLogin = 'http://explorargdb.somee.com/api/usuario/login';
+urlRegistro = 'https://explorargdb.somee.com/api/usuario';
+urlLogin = 'https://explorargdb.somee.com/api/usuario/login';
 
 // dashboard usuario
-putNombre = 'http://explorargdb.somee.com/api/usuario/editnombre';
-putEmail = 'http://explorargdb.somee.com/api/usuario/editmail';
-putPassw = 'http://explorargdb.somee.com/api/usuario/editpassw';
-gettingToken = 'http://explorargdb.somee.com/api/usuario/token';
+putNombre = 'https://explorargdb.somee.com/api/usuario/editnombre';
+putEmail = 'https://explorargdb.somee.com/api/usuario/editmail';
+putPassw = 'https://explorargdb.somee.com/api/usuario/editpassw';
+gettingToken = 'https://explorargdb.somee.com/api/usuario/token';
 
 // destinos
-urlDestinos = 'http://explorargdb.somee.com/api/destinos';
+urlDestinos = 'https://explorargdb.somee.com/api/destinos';
 urlDestino_Atraccion = 'http://explorargdb.somee.com/api/destino_atraccion';
 
 // atracciones
-urlAtracciones = 'http://explorargdb.somee.com/api/atracciones';
+urlAtracciones = 'https://explorargdb.somee.com/api/atracciones';
 
 // atractinos (atracciones y destinos)
-urlAtractinos = 'http://explorargdb.somee.com/api/atractinos';
+urlAtractinos = 'https://explorargdb.somee.com/api/atractinos';
 
 // openWeather
 openWeathUrl = 'https://api.openweathermap.org/data/2.5/weather?appid=9c52a24aa0e31859db10036c2852ef1f&units=metric&lang=sp&id=';
 
 // Foro
-urlForo = 'http://explorargdb.somee.com/api/foro';
-urlForoTopic = 'http://explorargdb.somee.com/api/foroTopic';
-urlForoCrud = 'http://explorargdb.somee.com/api/agregarTema';
+urlForo = 'https://explorargdb.somee.com/api/foro';
+urlForoTopic = 'https://explorargdb.somee.com/api/foroTopic';
+urlForoCrud = 'https://explorargdb.somee.com/api/agregarTema';
 
 // Chat
-urlChat = 'http://explorargdb.somee.com/api/chat';
+urlChat = 'https://explorargdb.somee.com/api/chat';
 }
